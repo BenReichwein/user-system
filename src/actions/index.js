db7663d49f1cf3177922f06d71bebbc81b9976d3
BenReichwein <ben.basque@icloud.com>
2021-02-25 11:12:24 -0500

    added comments to redux actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,6 +10,7 @@ import {
 //
 //-> Messages
 //
+// gets welcome message from api to check if api is running
 export const welcomeMessage = () => async (dispatch) => {
   const response = await api.get('home')
   const message = await response.data;
@@ -19,6 +20,7 @@ export const welcomeMessage = () => async (dispatch) => {
 //
 //-> Authentications
 //
+// making an account
 export const register = (formValues) => () => {
   api.post('user/register', {
     email: formValues.email,
@@ -35,7 +37,7 @@ export const register = (formValues) => () => {
     alert('Error, Try again later')
   })
 };
-
+// logging into existing account
 export const login = (formValues) => () => {
   api.post('user/login', {
     email: formValues.email,
@@ -50,7 +52,7 @@ export const login = (formValues) => () => {
     alert('Email or password is incorrect')
   })
 };
-
+// Logging out of account
 export const logout = () => async () => {
   const response = await api.get('user/logout')
   if (response.status === 200) {
@@ -60,14 +62,14 @@ export const logout = () => async () => {
     alert(`Error: ${response.error}`)
   }
 };
-
+// Loading profile information
 export const loadProfile = () => async (dispatch) => {
   const response = await api.get('user/profile')
   const info = await response.data
 
   dispatch({ type: LOAD_PROFILE, payload: info});
 };
-
+// Update profile information
 export const updateProfile = (formValues) => async () => {
   const response = await api.post('user/register', {
     email: formValues.email,
@@ -82,6 +84,7 @@ export const updateProfile = (formValues) => async () => {
 //
 //-> Posts
 //
+// Creating a post
 export const createPost = (formValues) => async (dispatch) => {
   const response = await api.post('posts', {
     title: formValues.title,
@@ -95,21 +98,21 @@ export const createPost = (formValues) => async (dispatch) => {
     alert('Error: you need to be logged in to post')
   }
 };
-
+// Get all the posts
 export const getPost = () => async (dispatch) => {
   const response = await api.get('posts')
   const post = await response.data;
 
   dispatch({ type: GET_POST, payload: post});
 };
-
+// Delete a single post
 export const deletePost = (id) => async (dispatch) => {
   const response = await api.delete(`posts/${id}`)
   const post = await response.data;
 
   dispatch({ type: GET_POST, payload: post});
 };
-
+// Comment on a post
 export const createComment = (comment, postUid, id) => async (dispatch) => {
   const response = await api.post(`posts/comment/${id}`, {
     postUid,
@@ -120,7 +123,7 @@ export const createComment = (comment, postUid, id) => async (dispatch) => {
 
   dispatch({ type: GET_POST, payload: post});
 }
-
+// Delete a comment (could be from owner of post or commenter)
 export const deleteComment = (comment, commentUid, id) => async (dispatch) => {
   const response = await api({
     method: 'delete',
@@ -138,35 +141,35 @@ export const deleteComment = (comment, commentUid, id) => async (dispatch) => {
 
   dispatch({ type: GET_POST, payload: post});
 };
-
+// Add post to users saved collection
 export const addSaved = (id) => async () => {
   const response = await api.post(`saved`, {
     postId: id
   })
   alert(response.data)
 }
-
+// Get all saved post from user
 export const getSaved = () => async (dispatch) => {
   const response = await api.get('saved')
   const post = await response.data;
 
   dispatch({ type: GET_POST, payload: post});
 };
-
+// Remove a saved post from users collection
 export const deleteSaved = (id) => async (dispatch) => {
   const response = await api.delete(`saved/${id}`)
   const post = await response.data;
 
   dispatch({ type: GET_POST, payload: post});
 }
-
+// Get all users posts that they've posted
 export const getUsersPosts = (uid) => async (dispatch) => {
   const response = await api.get(`userPosts/${uid}`)
   const post = await response.data;
 
   dispatch({ type: GET_POST, payload: post});
 }
-
+// Get user id from authentication
 export const getUid = () => async (dispatch) => {
   const response = await api.get(`user/getUid`)
   const uid = await response.data;
@@ -176,13 +179,14 @@ export const getUid = () => async (dispatch) => {
 //
 // -> Administrative Control
 //
+// Get all the accounts and their information
 export const loadAdmin = () => async (dispatch) => {
   const response = await api.get('admin/users')
   const users = await response.data;
 
   dispatch({ type: LOAD_ADMIN, payload: users});
 };
-
+// Delete single account
 export const deleteAdmin = (id) => async (dispatch) => {
   const response = await api.delete(`admin/${id}`)
   const users = await response.data;
